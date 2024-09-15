647a801eb6cf45718c6c57d46d96332b8799ee21
Shoriat Ullah <su.shoriat@gmail.com>
2024-09-16 03:59:09 +0600

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,199 @@
+
+$('.mobile-nav').hide();
+$('.hide').hide();
+
+
+$('.show').click (function(){
+  $('.mobile-nav').slideDown();
+  $('.show').hide();
+  $('.hide').show();
+})
+
+
+$('.hide').click (function(){
+  $('.mobile-nav').slideUp();
+  $('.show').show();
+  $('.hide').hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// frotpoli
+
+$('.websie-hide').hide();
+
+
+
+// mobile-all
+
+
+$('.all').click (function(){
+  $('.one-websie').show();
+  $('.websie-two').show();
+  $('.websie-there').show();
+  $('.websie-for').show();
+  $('.websie-five').show();
+  $('.websie-six').show();
+  $('.websie-seven').show();
+  $('.websie-eaite').show();
+  $('.websie-nine').show();
+  $('.websie-ten').show();
+  $('.websie-aleven').show();
+  $('.websie-towalve').show();
+})
+
+
+$('.Agency').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').show();
+  $('.websie-there').hide();
+  $('.websie-for').show();
+  $('.websie-five').hide();
+  $('.websie-six').hide();
+  $('.websie-seven').hide();
+  $('.websie-eaite').show();
+  $('.websie-nine').hide();
+  $('.websie-ten').show();
+  $('.websie-aleven').show();
+  $('.websie-towalve').hide();
+})
+
+
+$('.Education').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').hide();
+  $('.websie-there').hide();
+  $('.websie-for').hide();
+  $('.websie-five').hide();
+  $('.websie-six').show();
+  $('.websie-seven').hide();
+  $('.websie-eaite').hide();
+  $('.websie-nine').hide();
+  $('.websie-ten').hide();
+  $('.websie-aleven').hide();
+  $('.websie-towalve').hide();
+
+  $('.websie-hide').show();
+
+})
+
+
+$('.Landing').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').hide();
+  $('.websie-there').show();
+  $('.websie-for').show();
+  $('.websie-five').hide();
+  $('.websie-six').hide();
+  $('.websie-seven').hide();
+  $('.websie-eaite').hide();
+  $('.websie-nine').hide();
+  $('.websie-ten').hide();
+  $('.websie-aleven').show();
+  $('.websie-towalve').hide();
+
+  $('.websie-hide').show()
+})
+
+
+$('.Blog').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').hide();
+  $('.websie-there').hide();
+  $('.websie-for').hide();
+  $('.websie-five').show();
+  $('.websie-six').hide();
+  $('.websie-seven').hide();
+  $('.websie-eaite').hide();
+  $('.websie-nine').hide();
+  $('.websie-ten').hide();
+  $('.websie-aleven').hide();
+  $('.websie-towalve').hide();
+
+
+  $('.websie-hide').show()
+})
+
+
+$('.Furniture').click (function(){
+  $('.one-websie').show();
+  $('.websie-two').hide();
+  $('.websie-there').hide();
+  $('.websie-for').hide();
+  $('.websie-five').hide();
+  $('.websie-six').hide();
+  $('.websie-seven').hide();
+  $('.websie-eaite').hide();
+  $('.websie-nine').hide();
+  $('.websie-ten').hide();
+  $('.websie-aleven').hide();
+  $('.websie-towalve').hide();
+
+
+  $('.websie-hide').show()
+})
+
+
+$('.Resturet').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').hide();
+  $('.websie-there').hide();
+  $('.websie-for').hide();
+  $('.websie-five').hide();
+  $('.websie-six').hide();
+  $('.websie-seven').show();
+  $('.websie-eaite').hide();
+  $('.websie-nine').show();
+  $('.websie-ten').hide();
+  $('.websie-aleven').hide();
+  $('.websie-towalve').hide();
+
+
+  $('.websie-hide').show()
+})
+
+
+
+
+
+
+$('.Business').click (function(){
+  $('.one-websie').hide();
+  $('.websie-two').show();
+  $('.websie-there').hide();
+  $('.websie-for').show();
+  $('.websie-five').hide();
+  $('.websie-six').hide();
+  $('.websie-seven').hide();
+  $('.websie-eaite').show();
+  $('.websie-nine').hide();
+  $('.websie-ten').hide();
+  $('.websie-aleven').show();
+  $('.websie-towalve').hide();
+})
+
+
+
